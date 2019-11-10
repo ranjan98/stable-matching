124f3ad35167aa85f928d7afd2ec00881f38d07f
ranjangoyal1 <ranjangoyal1@gmail.com>
2019-11-10 13:34:45 +0530

    Update stablematching.py

--- stablematching.py
+++ stablematching.py
@@ -1,40 +1,45 @@
-def stableMatching(n,menPreferences,womenPreferences):
+#I solved this problem:
+def stableMatching(n, menPreferences, womenPreferences):
+# Do not change the function definition line.
+
+    # Initially, all n men are unmarried
     unmarriedMen = list(range(n))
+    # None of the men has a spouse yet, we denote this by the value None
     manSpouse = [None] * n                      
+    # None of the women has a spouse yet, we denote this by the value None
     womanSpouse = [None] * n                      
-    nextManChoice = [0] * n
-    pos=-1
-    husbandpos=-1
-    hepos=-1
-    while(unmarriedMen):
-        he = unmarriedMen[0]                    
+    # Each man made 0 proposals, which means that 
+    # his next proposal will be to the woman number 0 in his list
+    nextManChoice = [0] * n                       
+    
+    # While there exists at least one unmarried man:
+    while unmarriedMen:
+        # Pick an arbitrary unmarried man
+        he = unmarriedMen[0]                      
+        # Store his ranking in this variable for convenience
         hisPreferences = menPreferences[he]       
+        # Find a woman to propose to
         she = hisPreferences[nextManChoice[he]] 
+        # Store her ranking in this variable for convenience
         herPreferences = womenPreferences[she]
-        currentHusband = womanSpouse[she]
-        if(currentHusband==None):
-            manSpouse[he]=she
-            womanSpouse[she]=he
-            del unmarriedMen[0];
-            nextManChoice[he]+=1;
-        else:
-            for i in range(0,n):
-                if(herPreferences[i]==he):
-                    pos=i
-                if(herPreferences[i]==currentHusband):
-                    husbandpos=i
-            if(pos<husbandpos):
-                for i in range(0,n):
-                    if(manSpouse[i]==she):
-                        hepos=i
-                manSpouse[hepos]=None
-                del unmarriedMen[0];
-                womanSpouse[she]=he
-                manSpouse[he]=she
-                unmarriedMen.append(currentHusband)
-                nextManChoice[he]+=1
-            else:
-                nextManChoice[he]+=1
+        # Find the present husband of the selected woman (it might be None)
+        currentHusband = womanSpouse[she]         
+        
+        # Write your code here
+        
+        # Now "he" proposes to "she". 
+        # Decide whether "she" accepts, and update the following fields
+        # 1. manSpouse
+        # 2. womanSpouse
+        # 3. unmarriedMen
+        # 4. nextManChoice
+            
+    # Note that if you don't update the unmarriedMen list, 
+    # then this algorithm will run forever. 
+    # Thus, if you submit this default implementation,
+    # you may receive "SUBMIT ERROR".
     return manSpouse
-assert(stableMatching(1, [ [0] ], [ [0] ]) == [0])
-assert(stableMatching(2, [ [0,1], [1,0] ], [ [0,1], [1,0] ]) == [0, 1])
+    
+# You might want to test your implementation on the following two tests:
+# assert(stableMatching(1, [ [0] ], [ [0] ]) == [0])
+# assert(stableMatching(2, [ [0,1], [1,0] ], [ [0,1], [1,0] ]) == [0, 1])
